c81e6ebf0ddd54bb3f755c0355a79d166caf1050
Sachin Kumar <sachin@mono-cvs.ximian.com>
2004-05-12 12:08:16 +0000

    displayDetails.php: Added checks to make sure that input file is correct
    svn path=/trunk/release/; revision=27161

--- scripts/displayDetails.php
+++ scripts/displayDetails.php
@@ -5,6 +5,7 @@
 <head>
 	<title>Mono TestCase Results</title>
 	<?php
+		#This function fetches testcases from the given XML file based on the given testsuite and status
 		function fetch_testcases($testsuite_key,$status_key,$file) 
 		{
 			$doc = xmldocfile ($file);
@@ -18,7 +19,7 @@
 				if ($testsuite->get_attribute("name") == $testsuite_key) {
 					$logfile = $testsuite->get_elements_by_tagname("testsuite-log");
 	                                if (count($logfile) > 0) {
-						//If testsuite contains logfile details, return the log message
+						#If testsuite contains logfile details, return an array containing 0 and the log message
 						$sorted_testcases[0] = "0";
 						$log_content = preg_replace('"<"','&lt;',$logfile[0]->get_content());
 						$sorted_testcases[1] = $log_content;
@@ -49,6 +50,8 @@
 			sort ($sorted_testcases);
 			return $sorted_testcases;
 		}
+		# This function fetches the failed testcases of the recent run and the passed test cases of the previous run
+		# Those testcases that passed in the previous run and failed in the recent run are returned
 		function fetch_regressed_testcases ($testsuite_name,$file_1,$file_2)
                 {
                         $doc_1 = xmldocfile ($file_1);
@@ -68,6 +71,7 @@
                                 if ($testsuite_1->get_attribute("name") == $testsuite_name) {
                                         $testcases = array();
                                         $testcases = $testsuite_1->get_elements_by_tagname ("testcase");
+					#Store those testcases that failed in the recent run
                                         foreach ($testcases as $testcase) {
                                                 if ($testcase->get_attribute ("status") == "1") {
                                                         $testcases_1[$testcases_1_count] = $testcase->get_attribute("name");
@@ -79,13 +83,14 @@
                                 }
                         }
                         if ($testflag == 0)
-                                return $regressed_testcases; //return empty array
+                                return $regressed_testcases; #return empty array if no entry is present for the recent run
 			$testflag = 0;
                         $testcases_2_count = 0;
                         foreach ($testsuites_2 as $testsuite_2) {
                                 if ($testsuite_2->get_attribute("name") == $testsuite_name) {
                                         $testcases = array ();
                                         $testcases = $testsuite_2->get_elements_by_tagname("testcase");
+					#Store those testcases that passed in the previous run
                                         foreach ($testcases as $testcase) {
                                                 if ($testcase->get_attribute ("status") == "0") {
                                                         $testcases_2[$testcases_2_count] = $testcase->get_attribute("name");
@@ -97,17 +102,21 @@
                                 }
                         }
                         if ($testflag == 0)
-                                return $regressed_testcases; //return empty array
+                                return $regressed_testcases; #return empty array if no entry is present for the previous run
 
 			if (count($testcases_1) == 0 || count($testcases_2) == 0)
-                                return $regresses_testcases; //return empty array
-                                                                                                                             
+                                return $regresses_testcases; #return empty array
+
+
                         sort($testcases_1);
                         sort($testcases_2);
+
                         $testcases_1_count = 0;
                         $testcases_2_count = 0;
 			$regression_count = 0;
-                                                                                                                             
+
+                        
+			#Checking for regression
                         while ($testcases_1_count < count($testcases_1) && $testcases_2_count < count($testcases_2)) {
                                 if ($testcases_1[$testcases_1_count] == $testcases_2[$testcases_2_count]) {
                                         $regressed_testcases[$regression_count] = $testcases_1[$testcases_1_count];
@@ -125,6 +134,14 @@
                         return $regressed_testcases;
                                                                                                                              
                 }
+		#This function makes sure the input file names are correct 
+		function sanitize ($filename) {
+			if (!is_numeric($filename)) {
+				print "<b><font color = red>Wrong Filename</b></font>";
+				exit;
+			}
+			
+		}
 	?>
 </head>
                                                                                                                           
@@ -134,12 +151,25 @@
 		$testsuite_key = $_GET['testsuite'];
 		$status_key = $_GET['status'];
 		$file = $_GET['file'];
-		$file_1 = $_GET['file1'];
+	
+		#Checking whether the input file is correct
+		sanitize ($file);
 
-		$date = `date +"%Y%m%d"`;
-                $chart_url = "TestResults/Charts/$testsuite_key/$date.png";
+		#Prepending the base directory and appending .xml to the input filename
+		$root = "./";
+		$dir = $root."testresults/xml/";
+		$file = $dir . "testresults-" .$file . ".xml";
 
+		#Obtaining location of the recent chart for the particular testsuite
+		$chart_url = $root."testresults/charts/$testsuite_key".".png";
+
+		#Displaying regressed testcases
 		if ($_GET['regression'] == 1) {
+
+			$file_1 = $_GET['file1'];
+			sanitize ($file_1);//Checking whether input file1 is correct
+			$file_1 = $dir ."testresults-". $file_1 . ".xml";
+
 			print "<h3>REGRESSED TESTCASES FOR " .  $_GET['testsuite'] . "&nbsp;<a href=\"$chart_url\"><font size=\"2\" color=\"blue\">&lt;View Progress Chart></font></a></h3>";
 			$testcases = fetch_regressed_testcases($testsuite_key,$file,$file_1);
 			print "<table border=1><tr><th>S.No.</th><th>Test Case Name</th><th></tr>";	
@@ -150,14 +180,18 @@
 			}
 			return;
 		}
+
 		$testcases = fetch_testcases($testsuite_key,$status_key,$file);
 		if ($testcases[0] == "0") {
-			//Displaying log message
+
+			#Displaying log message
 			print "<h3>TESTCASE RESULTS FOR " .  $_GET['testsuite'] . "&nbsp;<a href=\"$chart_url\"><font size=\"2\" color=\"blue\">&lt;View Progress Chart></font></a></h3>";
 			print "<pre>".$testcases[1]."</pre>";
 			return;
 		}
-		if ($status_key == 0) //Pass Tests
+			
+		#Displaying passed testcases
+		if ($status_key == 0)
 		{
 			print "<h3>PASSED TESTCASES FOR " .  $_GET['testsuite'] . "&nbsp;<a href=\"$chart_url\"><font size=\"2\" color=\"blue\">&lt;View Progress Chart></font></a></h3>";
 			print "<table border=1><tr><th>S.No.</th><th>Test Case Name</th><th>Execution Time</th></tr>";
@@ -169,7 +203,8 @@
 			print "<table>";
 		}
 		
-		else if ($status_key == 2) //Not Run Tests
+		#Displaying not run testcases
+		else if ($status_key == 2) 
 		{
 			print "<h3>NOT RUN TESTCASES FOR " .  $_GET['testsuite'] . "&nbsp;<a href=\"$chart_url\"><font size=\"2\" color=\"blue\">&lt;View Progress Chart></font></a></h3>";
 			print "<table border=1><tr><th>S.No.</th><th>Test Case Name</th><th>Message</th></tr>";
@@ -181,7 +216,8 @@
 			print "</table>";
 		}
 		
-		else if ($status_key == 1) //Failed Test Case
+		#Displaying failed testcases
+		else if ($status_key == 1) 
 		{
 			print "<h3>FAILED TESTCASES FOR " .  $_GET['testsuite'] . "&nbsp;<a href=\"$chart_url\"><font size=\"2\" color=\"blue\">&lt;View Progress Chart></font></a></h3>";
 			print "<table border =1><tr><th><b>S.No.</th></b><th><b>Test Case Name</b></th><th><b>Message</b></th></tr>";
@@ -203,6 +239,7 @@
 				$count++;
 			}
 		}
+
 		if ($count == 1)
                         print "<font color=red><b> No Testcase Details Available! </b></font>";	
                                                                                                                              
