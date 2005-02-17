1160f8ffb3eaf5a086b53b423194591226eea857
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2005-02-17 23:23:24 +0000

    added script to build masterinfos on windows
    svn path=/trunk/release/; revision=40836

---
+++ masterinfos/createinfos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if test "$1" = "1.1" ; then
+	INFO=./mono-api-info.exe
+else
+	if test "$1" = "2.0" ; then
+		INFO=./mono-api-info2.exe
+	else
+		echo "Need 1.0 or 2.0"
+		exit 1
+	fi
+fi
+
+rm -rf $1
+mkdir $1 || exit 1
+
+for i in $(cat assemblies-list.txt)
+do
+	echo $i
+	$INFO $i > $1/$i.xml
+done
+
