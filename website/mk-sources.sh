8f1e0a79913cf6ce33f20c4e0b5e7404f806aa2f
Ben Maurer <benm@mono-cvs.ximian.com>
2005-05-20 16:47:32 +0000

    little script to make an index page for sources
    svn path=/trunk/release/; revision=44835

---
+++ website/mk-sources.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+confdir=$(dirname $(pwd)/$0)
+packagingdir=$confdir/../packaging
+. $packagingdir/shared-code.sh
+
+WEB_DIR=$1
+
+cd $WEB_DIR
+OUT=index.html
+rm $OUT
+
+cat $confdir/sources | while read line
+do
+	if [ "x${line:0:1}" == "x#" ]; then
+		ARGS=(${line:1})
+		TARBALLS=()
+		for package in ${ARGS[@]}; do 
+			
+			if [[ x$package == xgtk-sharp-2.0 ]]; then
+				latest_tarball "$package/gtk-sharp-1.9.*.tar.gz"
+				TARBALLS=(${TARBALLS[@]} $LATEST_VERSION)
+			fi
+			
+			latest_tarball "$package/*.tar.gz"
+			
+			TARBALLS=(${TARBALLS[@]} $LATEST_VERSION)
+		done
+		echo ${TARBALLS[@]}
+		for i in ${TARBALLS[@]}; do
+			n=$(basename $i)
+			echo "<li><a href='$i'>$n</a></li>" >> $OUT
+		done
+		
+		echo "</ul>" >> $OUT
+	else
+		echo ${line} >> $OUT
+	fi
+done
