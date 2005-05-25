2cfc69204dfe87081d2fd6cf0c18abfd21644749
Wade Berrier <wade@mono-cvs.ximian.com>
2005-05-25 07:05:56 +0000

    rpm-miguel.tcl -> buildlinux-installer.tcl
    buildlinux-installer.tcl:
            Revamp the tcl script from Daniel...
            
            It will get some stock os rpms (currently redhat9)
                    and then pull the latest rpms from local builds
    
            Will also download and install the installer if it isn't
            installed
    
            Tries to be dynamic from release to release, but the index.html
            for the installer "readme" will still need to be updated
                    manually
    
    
    svn path=/trunk/release/; revision=44975

--- linux-installer/buildlinux-installer.tcl
+++ linux-installer/buildlinux-installer.tcl
@@ -1,14 +1,54 @@
 #!/usr/bin/tclsh
 
-# Can this somehow get set from an environment variable?
-set version 1.1.7
+
+# Some history on this script...
+################################
+# Daniel Lopez used this script to create installers for Mono
+#
+# I then revamped some of it so it would dynamically get the latest packages that were built locally
+# 
+# It may be easier to convert it to bash or perl later if the tcl is a pain to maintain
+# 
 
 
+# This script will build with the latest rpms, but it needs the release version of mono
+set version [ lindex $argv 0]
+
+if { $version == "" } then {
+	puts "Usage: buildlinux-installer.tcl <mono release version>"
+	puts "  example: buildlinux-installer.tcl 1.1.7-1"
+	exit 1
+}
+
+puts "Version: $version"
+
+# Install the installer if it's not installed...
+if { ![file exists ~/installbuilder-2.4.1/bin/builder] } {
+
+
+	set installer installbuilder-multiplatform-2.4.1-linux-installer.bin
+
+	# Download it if it doesn't exist
+	if { ![file exists installbuilder-multiplatform-2.4.1-linux-installer.bin ] } {
+		puts "Downloading the Bitrock installer..."
+		catch {exec wget http://164.99.120.153/linux-installer/bitrock/$installer } results
+		puts $results
+	}
+
+	# Script will die here if the file can't be found...
+	file attributes $installer -permission 0755
+
+	puts "Installing the Bitrock installer..."
+	exec ./$installer --mode unattended 
+}
+
+# Get the current working directory, the tcl way :)
 set prefix [file normalize [file dirname [info script]]]
 
 array set ::opts \
     [list buildroot $prefix/build \
-    tarballs $prefix/thirdparty/tarballs]
+    external_rpms $prefix/thirdparty/external_rpms \
+    mono_rpms $prefix/../packaging/packages]
 
 file mkdir $opts(buildroot)
 file delete -force $opts(buildroot)/usr $opts(buildroot)/etc
@@ -49,47 +89,104 @@ namespace eval ::maui::file {
     }
 }
 
+# Takes a full path to an rpm, and will extract it into the buildroot
 proc extract {file} {
+
+    global opts
+    set cwd [exec pwd]
+    cd $opts(buildroot)
+    
+    puts "Extracting $file"
     exec rpm2cpio $file > kk
     catch {exec cpio --extract --make-directories < kk} kk
+    file delete -force kk
+
+    # Print out results
     puts $kk
+
+    cd $cwd
+
 }
 
 proc extractRPMs {base rpmList} {
     foreach f $rpmList {
-	foreach file [glob $base/${f}*] {
-	    puts "Extracting $file"
+
+	# ls has version sorting (-v), use that to get the latest version from the directory
+	set latest_version [ exec ls -vr $base | head -n1 ]
+
+	foreach file [glob $base/$latest_version/${f}*] {
 	    extract $file
 	}
     }
 }
 
 
-cd $opts(buildroot)
-
-foreach {base rpmList} \
-    [list $opts(tarballs)/www.go-mono.com/archive/1.0.5/redhat-9-i386/ {
-	libgtkhtml gtksourceview-1
-    } $opts(tarballs)/www.go-mono.com/archive/1.0.6/redhat-9-i386/ {
-	gtksourceview cairo gecko libpixman
-    } $opts(tarballs)/www.go-mono.com/archive/$version/redhat-9-i386/ {
-	mono-core mono-devel mono-locale mono-web mono-data bytefx ibm-data
-	mono-basic mono-ikvm mono-winforms mono-extras libgdiplus mono-jscript
-	gtk-sharp xsp mod_mono monodoc
-    } $opts(tarballs)/www.go-mono.com/archive/$version/suse-93-i586 {
-	gtk-sharp2
-    } $opts(tarballs)/ftp.ximian.com/pub/mono/redhat-9-i386/ {
-	libicu libstdc linc libgal pkgconfig
-    }] {	 
-	extractRPMs $base $rpmList		  
-	
-    }
 
 # http://ftp.ximian.com/pub/mono/redhat-9-i386/libicu26-2.6.2-1.ximian.6.0.i386.rpm
 # ftp://fr2.rpmfind.net/linux/redhat/9/en/os/i386/RedHat/RPMS/libstdc++-3.2.2-5.i386.rpm
 # ftp://fr2.rpmfind.net/linux/redhat/9/en/os/i386/RedHat/RPMS/linc-1.0.1-1.i386.rpm
 # ftp://fr.rpmfind.net/linux/redhat/9/en/os/i386/RedHat/RPMS/pkgconfig-0.14.0-3.i386.rpm
 # ftp://ftp.ximian.com/pub/ximian-evolution/redhat-9-i386/libgal2.0_6-1.99.11-0.ximian.6.1.i386.rpm
+
+
+# some stock os rpms
+set external_rpms_list {
+	http://fr2.rpmfind.net/linux/redhat/9/en/os/i386/RedHat/RPMS/libstdc++-3.2.2-5.i386.rpm
+	http://fr2.rpmfind.net/linux/redhat/9/en/os/i386/RedHat/RPMS/linc-1.0.1-1.i386.rpm
+	http://fr.rpmfind.net/linux/redhat/9/en/os/i386/RedHat/RPMS/pkgconfig-0.14.0-3.i386.rpm
+}
+
+
+# Download the stock rpms if they don't exist already and then extract them
+cd $opts(external_rpms)
+foreach rpm $external_rpms_list {
+
+	if { ![file exists [file tail $rpm]] } {
+		puts "Downloading: $rpm";
+		catch { exec wget $rpm }
+	} else {
+		#puts "File already exists!"
+	}
+	
+	# Get a full path to the rpm in the cwd based on the url name
+	set full_path_to_rpm [file normalize [file tail $rpm]]
+
+	extract $full_path_to_rpm
+}
+
+
+# Get a list of rpms that we want in this install from the local builds
+#  The first option is a directory of where to look for the rpms
+#    It will pick a subdirectory with the best latest version
+#    and then glob the files beginning with the second arg (list)
+
+foreach {base rpmList} \
+    [list $opts(mono_rpms)/x86/mono-1.1/ {
+	mono-extras mono-jscript mono-ikvm mono-core
+	mono-devel mono-winforms
+	bytefx-data-mysql mono-basic
+	mono-locale-extras mono-data
+	mono-complete ibm-data-db2 mono-web
+    } $opts(mono_rpms)/redhat-9-i386/mod_mono {
+	mod_mono
+    } $opts(mono_rpms)/redhat-9-i386/gtk-sharp {
+	gtk-sharp
+    } $opts(mono_rpms)/redhat-9-i386/libgdiplus-1.1 {
+	libgdiplus
+    } $opts(mono_rpms)/noarch/boo {
+	boo
+    } $opts(mono_rpms)/noarch/xsp {
+	xsp
+    } $opts(mono_rpms)/noarch/ikvm {
+	ikvm
+    } $opts(mono_rpms)/noarch/monodoc {
+	monodoc
+    } $opts(mono_rpms)/suse-93-i586/gtk-sharp-2.0/ {
+	gtk-sharp2
+    }] {
+	extractRPMs $base $rpmList		  
+    }
+
 
 proc recurseDirectories {path} {
     file attributes $path -permissions 0755
@@ -105,6 +202,7 @@ set p [file normalize $opts(buildroot)]
 file rename -force $opts(buildroot)/usr/bin/mono $opts(buildroot)/usr/bin/mono.bin
 file copy -force $p/../mono.template $opts(buildroot)/usr/bin/mono
 
+# Set of symbolic links
 foreach d [list 1.0 2.0 gtk-sharp gecko-sharp] {
     if {![file exists [file join $p usr lib mono $d]]} {
 	puts "Skipping $d"
@@ -182,9 +280,17 @@ file copy -force $p/../projects/mono/readme.gif $p/usr/share/pixmaps
 file copy -force $p/../projects/mono/monourl.gif $p/usr/share/pixmaps
 file copy -force $p/../projects/mono/monotm48x48.png $p/usr/share/pixmaps
 file copy -force $p/../projects/mono/License.txt $p/usr/share/doc
-file copy -force $p/../projects/mono/Readme-$version.txt $p/usr/share/doc/Readme.txt
+# Need to figure out another way to get this version specific file...
+#file copy -force $p/../projects/mono/Readme-1.1.7.txt $p/usr/share/doc/Readme.txt
+# Get the readme for this version if it's there, otherwise don't worry about it
+if { [ catch { exec wget http://go-mono.com/archive/$version -o $p/usr/share/doc/Readme.txt } ] } {
+	# If there was an error, zero out the Readme file
+	maui::file::write $p/usr/share/doc/Readme.txt ""
+
+}
 
 file mkdir $p/html
+# TODO: Here's another file that changes with each release... need to update it manually for now
 file copy -force $p/../projects/mono/html/index.html $p/html
 file copy -force $p/../projects/mono/html/mono.css $p/html
 file copy -force $p/../projects/mono/html/bitrock.png $p/html
@@ -204,5 +310,14 @@ maui::util::substituteParametersInFile $p/mono.xml \
 # Fix RPM directory permissions
 recurseDirectories $p/usr/
 
+# Do some safe cleanup
+file delete ~/installbuilder-2.4.1/output/mono-$version-installer.bin
+file delete $prefix/output/mono-$version-installer.bin
+
+# Run the install
 puts [exec ~/installbuilder-2.4.1/bin/builder build $p/mono.xml]
+
+# Move file to the current directory
+file rename ~/installbuilder-2.4.1/output/mono-$version-installer.bin $prefix/output
+
 
