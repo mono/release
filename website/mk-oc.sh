61630263e305535560ac7d89e96d7078f17b0e9b
Wade Berrier <wade@mono-cvs.ximian.com>
2005-12-02 23:59:54 +0000

    Add support for mono-deps channel (Holds rpms that don't come in the base distro that we need)
    some of our distros wouldn't install mono
    without some rpms.  Ex: sqlite, libunwind
    
    Add packages to packaging/external_packages/$DISTRO
    
    and they will get rsynced, and listed on the webpages,
    and placed in the mono-deps channel (yum and red-carpet)
    
    
    svn path=/trunk/release/; revision=53862

--- website/mk-oc.sh
+++ website/mk-oc.sh
@@ -25,6 +25,34 @@ cat $confdir/oc-config/$serverconf.conf | while read line; do
 	mkdir $chan
 	
 	cat $confdir/oc-config/$chan.chan | while read lline; do
+
+		# If line starts with '+'
+		if [ "x${lline:0:1}" == "x+" ]; then
+
+			external_dir=$(echo ${lline:1})
+
+			for distro_conf in $external_dir/*-*-* ; do
+
+				distro_info `basename $distro_conf`
+
+				mkdir -p $chan/$DISTRO
+
+				for rpm_file in $distro_conf/*.rpm ; do
+					# Skip source rpms
+					if [ ${rpm_file//\.src\.rpm/} != $rpm_file ] ; then
+						continue
+					fi
+
+					# Add rpm to the channel distro
+					ln $rpm_file $chan/$DISTRO
+				done
+
+			done
+			continue
+		fi
+
+
+		# If line doesn't start with '!' (Normal text)
 		if [ "x${lline:0:1}" != "x!" ]; then
 			echo ${lline//\\[\\[name\\]\\]/$serverconf} >> $chan/channel.conf
 			continue
@@ -38,7 +66,7 @@ cat $confdir/oc-config/$serverconf.conf | while read line; do
 		for distro_conf in $packagingdir/conf/*-*-*; do 
 
 			# Skip the distros that use zip packaging system
-			! egrep "^USE_ZIP_PKG" $packagingdir/conf/$distro_conf > /dev/null 2>&1 || continue
+			! egrep "^USE_ZIP_PKG" $distro_conf > /dev/null 2>&1 || continue
 
 			distro_info `basename $distro_conf`
 			ships_package || continue
@@ -53,6 +81,10 @@ cat $confdir/oc-config/$serverconf.conf | while read line; do
 	
 	
 	for i in $chan/*-*-*; do
+
+		# Skip distros that use zip packaging system
+		! egrep "^USE_ZIP_PKG" $i > /dev/null 2>&1 || continue
+
 		distro_info `basename $i`
 		
 		DISTRO_STRING=$DISTRO
