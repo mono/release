3a06847ef0f1ec699bb3734db08a956b8d40168f
Wade Berrier <wade@mono-cvs.ximian.com>
2006-02-01 03:51:26 +0000

    do-msvn-tar: export PKG_CONFIG_PATH so suse jail can find /opt/gnome/lib/pkgconfig utils.py: don't use read ahead buffer when launching processes
    svn path=/trunk/release/; revision=56372

--- pyutils/utils.py
+++ pyutils/utils.py
@@ -245,14 +245,13 @@ def launch_process(command, capture_stderr=1, print_output=1):
 	execute_option = ""
 	if capture_stderr: execute_option = " 2>&1 "
 
-	# bufsize=1 makes it line buffered, -1, unbuffered
-	#process = os.popen("%s %s" % ( command, execute_option), 'r', 1)
-	# I'm not noticing a difference
-	process = os.popen("%s %s" % ( command, execute_option), 'r', 1)
-
+	process = os.popen("%s %s" % ( command, execute_option) )
 	collected = []
-	# How to iterate over this so it's not so choppy?
-	for line in process:
+	# Use this looping method insead of 'for line in process' so it doesn't use the readahead buffer
+	#  This smooths output greatly, instead of getting big chunks of output with lots of lag
+	while(1):
+		line = process.readline()
+		if not line: break
 		if print_output:
 			print line,
 		collected += line
