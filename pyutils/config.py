bf8694f0c1ef5d82251964d985001194d574adc9
Wade Berrier <wade@mono-cvs.ximian.com>
2006-10-06 17:06:54 +0000

    Add the linux sparc and arm box to the mix
    svn path=/trunk/release/; revision=66364

--- pyutils/config.py
+++ pyutils/config.py
@@ -80,7 +80,7 @@ sd_sequential_build_distros = [ 'redhat-9-i386' ]
 sd_sequential_build_packages = [ 'mono', 'mono-1.1.13' ]
 
 # List of platforms/packages
-sd_latest_build_distros = [ 'redhat-9-i386', 'sles-9-x86_64', 'win-4-i386', 'macos-10-ppc', 'macos-10-x86', 'sunos-8-sparc', 'sunos-10-x86', 'sles-9-ia64', 'sles-9-s390', 'sles-9-s390x', 'sles-9-ppc', 'debian-3-arm', 'fedora-3-i386', 'nld-9-i586', 'suse-93-i586' ]
+sd_latest_build_distros = [ 'redhat-9-i386', 'sles-9-x86_64', 'win-4-i386', 'macos-10-ppc', 'macos-10-x86', 'sunos-8-sparc', 'sunos-10-x86', 'sles-9-ia64', 'sles-9-s390', 'sles-9-s390x', 'sles-9-ppc', 'debian-31-arm', 'debian-31-sparc', 'fedora-3-i386', 'nld-9-i586', 'suse-93-i586' ]
 sd_latest_build_packages = td_packages
 ##############################################
 
