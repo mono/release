8dde00ce1426fa92c27e41a2486071dab7970049
Wade Berrier <wade@mono-cvs.ximian.com>
2006-10-31 22:33:54 +0000

    locked_package_sled10.py: -sort names, remove duplicates -add yast2-online-update-frontend
    svn path=/trunk/release/; revision=67196

--- vmware_image/locked_packages_sled10.py
+++ vmware_image/locked_packages_sled10.py
@@ -1,23 +1,28 @@
 locked_packages = """
-	beagle
-	beagle-gui
-	tomboy
-	banshee
-	mono
-	lsb
-	f-spot
+	IronPython
 	MozillaFirefox
-	kernel-default
-	grub
 	NetworkManager
 	aaa_base
 	aaa_skel
+	autoconf
+	automake
+	banshee
+	bash
+	beagle
+	beagle-gui
+	bitstream
+	boo
+	bootsplash
+	compiz
+	coreutils
 	dos2unix
 	evince
-	libzypp
+	f-spot
 	file-roller
 	flash-player
+	font
 	gcalctool
+	gcc
 	gconf-editor
 	gdb
 	gdm
@@ -25,73 +30,67 @@ locked_packages = """
 	gftp
 	glibc-locale
 	gnome-applets
-	gnome-applets
+	gnome-main-menu
 	gnome-session
 	gnome-utils
-	coreutils
-	flash-player
-	gnome-main-menu
-	openssh
-	unzip
-	xsp
-	vim
-	yast2-packager
-	font
-	sharp
-	bash
-	bootsplash
-	samba
-	compiz
-	xorg-x11-server
+	grub
 	gstreamer
 	gtk2-themes
-	syslog-ng
+	heap-buddy
+	kbd
+	kernel-default
 	libgdiplus
-	tcpdump
+	libgphoto2
+	libtool
+	libzypp
 	log4net
+	lsb
+	lukemftp
+	mono
+	mysql
+	nant
+	nautilus-open-terminal
+	openssh
+	phpMyAdmin
+	postgresql
+	postgresql-server
 	preload
+	python-2
+	python-elementtree
 	resapplet
-	screen
-	sudo
+	rpm-python
 	rsh
+	samba
+	screen
+	sharp
+	strace
+	subversion
+	sudo
+	syslog-ng
+	tcl
+	tcpdump
 	telnet
 	terminfo
+	timezone
+	tomboy
+	unzip
+	update-alternatives
+	vim
 	wget
 	xgl
+	xntp
+	xorg-x11-driver-video
+	xorg-x11-server
+	xsp
+	yast2-firewall
 	yast2-ncurses
+	yast2-packager
+	yast2-online-update-frontend
 	yast2-qt
 	yast2-runlevel
+	yast2-sound
 	yast2-trans-en_US
 	zip
-	bitstream
-	kbd
-	lukemftp
-	boo
-	IronPython
-	nant
-	heap-buddy
-	update-alternatives
-	timezone
-	strace
-	automake
-	autoconf
-	gcc
-	libtool
-	python-2
-	xorg-x11-driver-video
-	libgphoto2
-	nautilus-open-terminal
-	yast2-sound
-	yast2-firewall
-	mysql
-	subversion
-	xntp
-	tcl
-	postgresql-server
-	postgresql
-	rpm-python
-	python-elementtree
-	phpMyAdmin
 """.split()
 
 
