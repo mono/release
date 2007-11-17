712edea587019c3e3f0864c2e7412034ef360289
Andrew Jorgensen <ajorgensen@novell.com>
2007-11-17 00:21:13 +0000

    Scripts for signing XPIs
    svn path=/trunk/release/; revision=89819

---
+++ moonlight/sign-xpis.cmd
@@ -0,0 +1,18 @@
+@ECHO OFF
+SETLOCAL
+SET SIGNBASE=C:\CodeSigning\SignTools
+SET PATH=%SIGNBASE%\nss\bin;%PATH%
+
+ECHO Signing Mozilla Plugin
+SET CERT="Novell"
+SET DB=%SIGNBASE%\nss\privkey
+SET NAME="novell-moonlight"
+
+FOR %%V IN ("1.0" "1.1") DO (
+	FOR %%A IN ("i386" "x86_64") DO (
+		signtool -d %DB% -k %CERT% -p %1 -X -Z %NAME%-%%V-%%A.xpi -c 9 -i install.js %NAME%-%%V-%%A
+	)
+)
+
+ENDLOCAL
+PAUSE
