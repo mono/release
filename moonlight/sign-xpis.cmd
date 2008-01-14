f1d2e3b017e1a3d14a8555edb78ece918bf10a17
Andrew Jorgensen <ajorgensen@novell.com>
2008-01-14 17:24:58 +0000

    revert 1.1 -> 2.0 change
    svn path=/trunk/release/; revision=92881

--- moonlight/sign-xpis.cmd
+++ moonlight/sign-xpis.cmd
@@ -8,7 +8,7 @@ SET CERT="Novell"
 SET DB=%SIGNBASE%\nss\privkey
 SET NAME="novell-moonlight"
 
-FOR %%V IN ("1.0" "2.0") DO (
+FOR %%V IN ("1.0" "1.1") DO (
 	FOR %%A IN ("i386" "x86_64") DO (
 		signtool -d %DB% -k %CERT% -p %1 -X -Z %NAME%-%%V-%%A.xpi -c 9 -i install.js %NAME%-%%V-%%A
 	)
