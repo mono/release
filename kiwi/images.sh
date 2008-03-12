85c87e2dd420365c784ba8e1be1c0820a1cb2c72
Andrew Jorgensen <ajorgensen@novell.com>
2008-03-12 17:14:23 +0000

    Various fixes, yeah I know these are crappy commit logs
    svn path=/trunk/release/; revision=98067

--- kiwi/images.sh
+++ kiwi/images.sh
@@ -2,18 +2,25 @@
 test -f /.kconfig && . /.kconfig
 test -f /.profile && . /.profile
 
-echo "Type: $type" > /type
-
 if [ "$type" = "vmx" ]; then
 # Kiwi doesn't give us swap
+	mount -o bind /lib/udev/devices /dev
 	dd if=/dev/zero of=/swap bs=1024 count=512000
-	mkswap /swap
+	/sbin/mkswap /swap
 	echo "/swap swap swap defaults 0 0" >> /etc/fstab
+	umount /dev
 fi
 
 if [ "$type" = "iso" ]; then
+	sed -i '/^\/swap/d' /etc/fstab
 	rm -f /swap
-	rpm -ev kernel-source
+	if rpm -q kernel-source; then
+		rpm -ev kernel-source
+	fi
 fi
 
-rpm -ev $delete
+for package in $delete; do
+	if rpm -q $package; then
+		rpm -ev $package
+	fi
+done
