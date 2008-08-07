55f5143ca3e1b10954631d2e8b8d901b507a2fd8
Rusty Howell <rhowell@mono-cvs.ximian.com>
2008-08-07 22:03:04 +0000

    * Update to signing script
    svn path=/trunk/release/; revision=109916

--- moonlight/sign-xpis.cmd
+++ moonlight/sign-xpis.cmd
@@ -1,5 +1,16 @@
 @ECHO OFF
 SETLOCAL
+
+IF "%1" == "" GOTO PROMPTPASS
+SET PWD=%1
+GOTO RUN
+
+:PROMPTPASS
+ECHO Enter code signing password:
+SET /p PWD=
+
+:RUN
+
 SET SIGNBASE=C:\CodeSigning\SignTools
 SET PATH=%SIGNBASE%\nss\bin;%PATH%
 
@@ -10,7 +21,7 @@ SET NAME="novell-moonlight"
 
 FOR %%V IN ("1.0" "2.0") DO (
 	FOR %%A IN ("i586" "x86_64") DO (
-		signtool -d %DB% -k %CERT% -p %1 -X -Z %NAME%-%%V-%%A.xpi -c 9 %NAME%-%%V-%%A
+		%SIGNBASE%\nss\bin\signtool.exe -d %DB% -k %CERT% -p %PWD% -X -Z %NAME%-%%V-%%A.xpi -c 9 %NAME%-%%V-%%A
 	)
 )
 
