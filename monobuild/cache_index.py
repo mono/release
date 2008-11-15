e720a8ad478e14bbfa47915deec52f810791cf74
Andrew Jorgensen <ajorgensen@novell.com>
2008-11-15 00:51:52 +0000

    make this use a commandline parameter instead
    svn path=/trunk/release/; revision=118909

--- monobuild/cache_index.py
+++ monobuild/cache_index.py
@@ -7,5 +7,5 @@ sys.path.append('../pyutils')
 
 import config
 
-os.system('curl http://localhost%s/python/monobuild.py | sed  "s/monobuild.py/python\/monobuild.py/g" > www/index.html.new; mv www/index.html.new www/index.html' % config.web_root_url)
+os.system('curl %s | sed  "s/monobuild.py/python\/monobuild.py/g" > www/index.html.new; mv www/index.html.new www/index.html' % sys.argv[1])
 
