4f5bf484c7d38b7117d82b515914873eaafc6268
Rusty Howell <rhowell@mono-cvs.ximian.com>
2008-12-09 17:19:45 +0000

    Added more files to MoonlightStatus site
    svn path=/trunk/release/; revision=121121

---
+++ website/MoonlightStatus/MoonlightStatus/Global.asax.cs
@@ -0,0 +1,52 @@
+// Global.asax.cs created with MonoDevelop
+// User: rhowell at 4:39 PM 12/8/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Web;
+using System.Web.SessionState;
+
+namespace MoonlightStatus
+{
+	
+	
+	public class Global : System.Web.HttpApplication
+	{
+		
+		protected virtual void Application_Start(object sender, EventArgs e)
+		{
+		}
+		
+		protected virtual void Session_Start(object sender, EventArgs e)
+		{
+		}
+		
+		protected virtual void Application_BeginRequest(object sender, EventArgs e)
+		{
+		}
+		
+		protected virtual void Application_EndRequest(object sender, EventArgs e)
+		{
+		}
+		
+		protected virtual void Application_AuthenticateRequest(object sender, EventArgs e)
+		{
+		}
+		
+		protected virtual void Application_Error(object sender, EventArgs e)
+		{
+		}
+		
+		protected virtual void Session_End(object sender, EventArgs e)
+		{
+		}
+		
+		protected virtual void Application_End(object sender, EventArgs e)
+		{
+		}
+	}
+}
