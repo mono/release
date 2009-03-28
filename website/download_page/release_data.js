04e01135849be5ad00efaf703b14de2d754961d8
Andrew Jorgensen <ajorgensen@novell.com>
2009-03-28 03:46:34 +0000

    update for 2.4 release
    svn path=/trunk/release/; revision=130455

--- website/download_page/release_data.js
+++ website/download_page/release_data.js
@@ -1,15 +1,15 @@
-var i586 = "x86 32bit (i586)";
-var x86_64 = "x86 64bit (x86_64)";
-var i586_x86_64 = "x86 32 and 64-bit (i586, x86_64)";
-var ppc = "PowerPC 32bit (ppc32)";
-var ia64 = "Itanium (ia64)";
-var s390 = "IBM s390 32bit";
-var s390x = "IBM s390x 64bit";
+var yast = "This distribution supports installing packages via YaST. Add the following installation source to YaST:"
+var yast_help = "For assistance with using repositories and installing packages with YaST, <a href='http://en.opensuse.org/Add_Package_Repositories_to_YaST'>visit the Yast help page.</a>"
+var zypper = "This distribution supports installing packages via Zypper. Add the following repository to Zypper:"
+var zypper_help = "<p>To add the repository, execute the following commands (as root):<blockquote><code>zypper addrepo <em>&lt;URL&gt;</em> mono-stable<br/>zypper refresh --repo mono-stable<br/>zypper dist-upgrade --repo mono-stable</code></blockquote></p>"
 var discontinued = "<div style=\"color: darkred; font-weight: bold\">Binaries for this platform have been discontinued.  Builds may be available from <a href=\"http://www.mono-project.com/Other_Downloads\">unsupported downloads</a> or we may be looking for a volunteer to maintain packages.</div>";
+var i586_x86_64_ia64 = "i586, x86_64, and  ia64"
+var i586_x86_64_ppc_ppc64_ia64 = "i586, x86_64, ppc, ppc64, and ia64"
+var enterprise = "<p>The <a href=\"http://www.novell.com/products/mono/\">SUSE Linux Enterprise Mono Extension</a> is available for purchase from <a href=\"http://www.novell.com/\">Novell</a>.</p>"
 
 var data =
 {
-	"release" : "2.2",
+	"release" : "2.4",
 	"platforms" : [
 	{
 		"name" : "VMware",
@@ -17,12 +17,12 @@ var data =
 		"dlicon" : "vmware_icon.jpg",
 		"version" : [
 		{
-			"name" : "openSUSE 11.0",
+			"name" : "openSUSE 11.1",
 			"arch" : [
 			{
-				"name" : "Mono 2.2",
+				"name" : "Mono 2.4",
 				"desc" : "",
-				"downloadText" : "Download the openSUSE 11.0 VMWare image which includes Mono 2.2<br/><a href=\"http://susestudio.com\"><img title=\"Built with SUSE Studio\" src=\"http://susestudio.com/images/built-with-web.png\" width=\"120\" height=\"30\" alt=\"Built with SUSE Studio\" align=\"right\"></a><ul><li><a href='http://ftp.novell.com/pub/mono/vmware/Mono-2.2_openSUSE-11.0.zip.torrent'>via Torrent</a> <li><a href='http://ftp.novell.com/pub/mono/vmware/Mono-2.2_openSUSE-11.0.zip'>via http</a> </ul><a href='http://mono-project.com/VMware_Image'>Instructions for using the VMware image</a>."
+				"downloadText" : "Download the openSUSE 11.1 VMWare image which includes Mono 2.4<br/><a href=\"http://susestudio.com\"><img title=\"Built with SUSE Studio\" src=\"http://susestudio.com/images/built-with-web.png\" width=\"120\" height=\"30\" alt=\"Built with SUSE Studio\" align=\"right\"></a><ul><li><a href='http://ftp.novell.com/pub/mono/vmware/Mono-2.4_openSUSE-11.1.zip.torrent'>via Torrent</a> <li><a href='http://ftp.novell.com/pub/mono/vmware/Mono-2.4_openSUSE-11.1.zip'>via http</a> </ul><a href='http://mono-project.com/VMware_Image'>Instructions for using the VMware image</a>."
 			}
 			]
 		}
@@ -34,12 +34,12 @@ var data =
 		"dlicon" : "livecd.jpg",
 		"version" : [
 		{
-			"name" : "openSUSE 11.0 Live CD",
+			"name" : "openSUSE 11.1 Live CD",
 			"arch" : [
 			{
-				"name" : "Mono 2.2",
+				"name" : "Mono 2.4",
 				"desc" : "",
-				"downloadText" : "Download the openSUSE 11.0 Live CD which includes Mono 2.2<br/><a href=\"http://susestudio.com\"><img title=\"Built with SUSE Studio\" src=\"http://susestudio.com/images/built-with-web.png\" width=\"120\" height=\"30\" alt=\"Built with SUSE Studio\" align=\"right\"></a><ul><li><a href='http://ftp.novell.com/pub/mono/livecd/Mono-2.2_openSUSE-11.0.iso.torrent'>via Torrent</a> <li><a href='http://ftp.novell.com/pub/mono/livecd/Mono-2.2_openSUSE-11.0.iso'>via http</a> </ul>"
+				"downloadText" : "Download the openSUSE 11.1 Live CD which includes Mono 2.4<br/><a href=\"http://susestudio.com\"><img title=\"Built with SUSE Studio\" src=\"http://susestudio.com/images/built-with-web.png\" width=\"120\" height=\"30\" alt=\"Built with SUSE Studio\" align=\"right\"></a><ul><li><a href='http://ftp.novell.com/pub/mono/livecd/Mono-2.4_openSUSE-11.1.iso.torrent'>via Torrent</a> <li><a href='http://ftp.novell.com/pub/mono/livecd/Mono-2.4_openSUSE-11.1.iso'>via http</a> </ul>"
 			}
 			]
 		}
@@ -54,9 +54,9 @@ var data =
 			"name" : "openSUSE 10.3",
 			"arch" : [
 			{
-				"name" : i586_x86_64,
+				"name" : i586_x86_64_ia64,
 				"desc" : "",
-				"downloadText" : "This distro supports installing packages via YaST. Add the following installation source to YaST:<ul><li><a href=\"http://ftp.novell.com/pub/mono/download-stable/openSUSE_10.3\">http://ftp.novell.com/pub/mono/download-stable/openSUSE_10.3</a></ul>For assistance with using repositories and installing packages with YaST, <a href='http://en.opensuse.org/Add_Package_Repositories_to_YaST'>visit the Yast help page.</a>"
+				"downloadText" : yast + "<ul><li><a href=\"http://ftp.novell.com/pub/mono/download-stable/openSUSE_10.3\">http://ftp.novell.com/pub/mono/download-stable/openSUSE_10.3</a></ul>" + yast_help
 			}
 			]
 		},
@@ -64,9 +64,9 @@ var data =
 			"name" : "openSUSE 11.0",
 			"arch" : [
 			{
-				"name" : i586_x86_64,
+				"name" : i586_x86_64_ia64,
 				"desc" : "",
-				"downloadText" : "This distro supports installing packages via YaST. Add the following installation source to YaST:<ul><li><a href='" + "http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.0'>http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.0" + "</a></ul> For assistance with using repositories and installing packages with YaST, <a href='http://en.opensuse.org/Add_Package_Repositories_to_YaST'>visit the Yast help page.</a>"
+				"downloadText" : zypper + "<ul><li><a href=\"http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.0\">http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.0</a></ul>" + zypper_help
 			}
 			]
 		},
@@ -74,9 +74,9 @@ var data =
 			"name" : "openSUSE 11.1",
 			"arch" : [
 			{
-				"name" : i586_x86_64,
+				"name" : i586_x86_64_ia64,
 				"desc" : "",
-				"downloadText" : "This distro supports installing packages via YaST. Add the following installation source to YaST:<ul><li><a href='" + "http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.1'>http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.1" + "</a></ul> For assistance with using repositories and installing packages with YaST, <a href='http://en.opensuse.org/Add_Package_Repositories_to_YaST'>visit the Yast help page.</a>"
+				"downloadText" : zypper + "<ul><li><a href=\"http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.1\">http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.1</a></ul>" + zypper_help
 			}
 			]
 		}
@@ -92,9 +92,29 @@ var data =
 			"name" : "Suse Linux Enterprise 10",
 			"arch" : [
 			{
-				"name" : i586_x86_64,
+				"name" : "Enterprise support for i586, x86_64, and s390x",
 				"desc" : "",
-				"downloadText" : "This distro supports installing packages via YaST. Add the following installation source to YaST:<ul><li><a href='" + "http://ftp.novell.com/pub/mono/download-stable/SLE_10'>http://ftp.novell.com/pub/mono/download-stable/SLE_10" + "</a></ul>For assistance with using repositories and installing packages with YaST, <a href='http://en.opensuse.org/Add_Package_Repositories_to_YaST'>visit the Yast help page.</a>"
+				"downloadText" : enterprise
+			},
+			{
+				"name" : i586_x86_64_ia64,
+				"desc" : "",
+				"downloadText" : yast + "<ul><li><a href=\"http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.1\">http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.1</a></ul>" + yast_help
+			}
+			]
+		},
+		{
+			"name" : "Suse Linux Enterprise 11",
+			"arch" : [
+			{
+				"name" : "Enterprise support for i586, x86_64, and s390x",
+				"desc" : "",
+				"downloadText" : enterprise
+			},
+			{
+				"name" : i586_x86_64_ppc_ppc64_ia64,
+				"desc" : "",
+				"downloadText" : zypper + "<ul><li><a href=\"http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.1\">http://ftp.novell.com/pub/mono/download-stable/openSUSE_11.1</a></ul>" + zypper_help
 			}
 			]
 		}
@@ -112,7 +132,7 @@ var data =
 			{
 				"name" : "All",
 				"desc" : "This download works on all versions of Windows 2000, XP, 2003 and Vista.",
-				"downloadText" : "<ul><li><a href='ftp://ftp.novell.com/pub/mono/archive/2.2/windows-installer/5/mono-2.2-gtksharp-2.12.7-win32-5.exe'>Mono for Windows, Gtk#, and XSP</a></li><li><a href=\"http://ftp.novell.com/pub/mono/gtk-sharp/\">Gtk# for .NET</a></li><li><a href='http://mono-project.com/MoMA'>Mono Migration Analyzer</a></li></ul>"
+				"downloadText" : "<ul><li><a href='http://ftp.novell.com/pub/mono/archive/2.4/windows-installer/6/mono-2.4-gtksharp-2.12.8-win32-6.exe'>Mono for Windows, Gtk#, and XSP</a></li><li><a href=\"http://ftp.novell.com/pub/mono/gtk-sharp/\">Gtk# for .NET</a></li><li><a href='http://mono-project.com/MoMA'>Mono Migration Analyzer</a></li></ul>"
 			}
 			]
 		}
@@ -130,49 +150,29 @@ var data =
 			{
 				"name" : "All",
 				"desc" : "This download works on Mac OS X Tiger (10.4) and Leopard (10.5).",
-				"downloadText" : "Includes Mono, Cocoa#, Gtk# installs in /Library/Frameworks:<ul><li><a href='http://ftp.novell.com/pub/mono/archive/2.2/macos-10-universal/5/MonoFramework-2.2_5.macos10.novell.universal.dmg'>Mono 2.2_5 Framework - Universal</a></li><li><a href=\"http://ftp.novell.com/pub/mono/monodevelop/MonoDevelop-1.9.1-1.dmg\">MonoDevelop 2.0 Alpha 2</a></li></li><li><a href='http://go-mono.com/sources/cocoa-sharp/cocoa-sharp-0.9.4.tar.bz2'>Cocoa# 0.9.4 source</a></ul>Gtk# and System.Windows.Forms applications require X11.  Installing on a machine without X11 installed will result in errors during install, and these components will not function correctly."
-			}
-			]
-		}
-		]
-	},
-
-	{
-		"name" : "RedHat",
-		"icon" : "http://www.mono-project.com/files/6/6e/Mono_icon_redhat.gif",
-		"dlicon" : "http://www.mono-project.com/files/6/6e/Mono_icon_redhat.gif",
-		"desc" : "This download works on RedHat Enterprise Linux 4 on x86 32bit (i586).",
-		"version" : [
-		{
-			"name" : "RedHat Enterprise Linux 4",
-			"arch" : [
-			{
-				"name" : i586,
-				"desc" : discontinued + "This download works on RedHat Enterprise Linux 4 on " + i586 + ".",
-				//"downloadText" : "The downloads are available <a href='http://ftp.novell.com/pub/mono/download-stable/rhel-4-i386'>here</a>."
-				"downloadText" : "The prefered method for installing Mono on RedHat is to use yum. Once yum is installed, putting the <a href='http://ftp.novell.com/pub/mono/download-stable/rhel-4-i386/mono.repo'>mono.repo</a> file in /etc/yum.repos.d will allow you to install mono and related packages.<br><br>For individual packages, <a href='http://ftp.novell.com/pub/mono/download-stable/rhel-4-i386'>go here</a>.<br><br>Note: You can get yum for RHEL from the <a href='http://www.centos.org/'>CentOS project</a>."
-			}
-			]
-		}
-		]
-	},
-	{
-		"name" : "Solaris",
-		"icon" : "http://www.mono-project.com/files/2/2f/Mono_icon_solaris.gif",
-		"dlicon" : "http://www.mono-project.com/files/2/2f/Mono_icon_solaris.gif",
-		"version" : [
-		{
-			"name" : "Solaris 8",
-			"arch" : [
-			{
-				"name" : "SPARC",
-				"desc" : discontinued + "This download works on Solaris 8 on SPARC.",
-				"downloadText" : "<a href='http://mono-project.com/SolarisInstructions'>Solaris package instructions<ul><li><a href='http://ftp.novell.com/pub/mono/archive/1.2.4/sunos-8-sparc/4/mono-1.2.4_4.sunos8.novell.sparc.pkg.gz'>Mono 1.2.4_4 Package</a></ul>"
+				"downloadText" : "Includes Mono, Cocoa#, Gtk# installs in /Library/Frameworks:<ul><li><a href='http://ftp.novell.com/pub/mono/archive/2.4/macos-10-universal/7/MonoFramework-2.4_7.macos10.novell.universal.dmg'>Mono 2.4_7 Framework - Universal</a></li><li><a href=\"http://ftp.novell.com/pub/mono/monodevelop/MonoDevelop-1.9.1-1.dmg\">MonoDevelop 2.0 Alpha 2</a></li></li><li><a href='http://go-mono.com/sources/cocoa-sharp/cocoa-sharp-0.9.4.tar.bz2'>Cocoa# 0.9.4 source</a></ul>Gtk# and System.Windows.Forms applications require X11.  Installing on a machine without X11 installed will result in errors during install, and these components will not function correctly."
 			}
 			]
 		}
 		]
 	}
 
+/*	{
+		"name" : "RedHat",
+		"icon" : "http://www.mono-project.com/files/6/6e/Mono_icon_redhat.gif",
+		"dlicon" : "http://www.mono-project.com/files/6/6e/Mono_icon_redhat.gif",
+		"version" : [
+		{
+			"name" : "RedHat Enterprise Linux",
+			"arch" : [
+			{
+				"name" : "All",
+				"desc" : discontinued,
+				"downloadText" : ""
+			}
+			]
+		}
+		]
+	}*/
 	]
 };
