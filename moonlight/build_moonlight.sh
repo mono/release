eb1e11ce66d8429a79e8ad094858643ae32dec27
Rusty Howell <rhowell@mono-cvs.ximian.com>
2009-05-15 22:37:40 +0000

    * fix build_moonlight
    svn path=/trunk/release/; revision=134255

--- moonlight/build_moonlight.sh
+++ moonlight/build_moonlight.sh
@@ -5,6 +5,8 @@ VERSIONS=$(cat VERSIONS)  #read versions from file
 PREVIEW=$(echo $VERSIONS | awk '{print $NF}')
 ARCH=$(uname -m | sed -e 's/i.86/i586/')
 
+SRCDIR=$HOME/src
+
 
 function fail
 {
@@ -14,13 +16,13 @@ function fail
 
 . ~/mono-dev-env || fail "Missing mono-dev-env"
 
-cd ~/src
+cd $SRCDIR
 svn co svn://anonsvn.mono-project.com/source/branches/moon/$PREVIEW
 
-cd $PREVIEW/mono
+cd $SRCDIR/$PREVIEW/mono
 ./autogen.sh && make && sudo make install || fail
 
-cd $PREVIEW/moon
+cd $SRCDIR/$PREVIEW/moon
 ./autogen.sh --without-performance --without-testing && make && sudo make install || fail
 make user-plugin || fail
 cp plugin/install/novell-moonlight.xpi ~/novell-moonlight-$PREVIEW-$ARCH.xpi
