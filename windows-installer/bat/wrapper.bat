51e44b52867d459ae08c2dbda74bbd7b683971c3
Andrew Jorgensen <ajorgensen@novell.com>
2009-06-11 19:43:03 +0000

    Use MONO_OPTIONS in wrapper scripts on windows
    svn path=/trunk/release/; revision=135945

--- windows-installer/bat/wrapper.bat
+++ windows-installer/bat/wrapper.bat
@@ -1,1 +1,1 @@
-@"@@DOS_MONO_INST_DIR@@\bin\mono.exe" "@@DOS_MONO_INST_DIR@@\@@MONO_WRAPPER@@.exe" %*
+@"@@DOS_MONO_INST_DIR@@\bin\mono.exe" %MONO_OPTIONS% "@@DOS_MONO_INST_DIR@@\@@MONO_WRAPPER@@.exe" %*
