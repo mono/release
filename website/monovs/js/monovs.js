c27c2c9e4662d22aa0e5636b596d4886749ea846
Jakub Steiner <jimmac@mono-cvs.ximian.com>
2010-05-25 13:58:57 +0000

    clean up the DOM mangling for features
    svn path=/trunk/release/; revision=157851

--- website/monovs/js/monovs.js
+++ website/monovs/js/monovs.js
@@ -3,13 +3,11 @@ $(document).ready(function () {
   $.gaTracker('UA-76510-1');
   $('.feature').addClass('jquery').find('a')
     .each(function () {
-      $(this).attr('href', function (href) {
-        return $(this).attr('href') + '?nc=1';
-      });
-    })
-    .each(function () {
-      var link = $(this).attr('href');
-      $(this).parents('.feature').wrap($('<a>', { 'href': link, 'rel': 'feat' })).parents('a').fancybox({
+      var href = $(this).attr('href') + '?nc=1',
+      heading = $(this).text();
+      $(this).parent("h2").text(heading).parent('.feature')
+        .wrap($('<a>', { 'href': href, 'rel': 'feat' }))
+        .parent('a').fancybox({
         'cyclic': true,
         'overlayShow': false,
         'speedIn': 200,
@@ -24,5 +22,5 @@ $(document).ready(function () {
 //        'overlayColor': '#000',
 //        'overlayOpacity': 0.7
       });
-    });
+  });
 });
