632b673b715c3c0daf1690aa6ae5e63c0a07d5e0
Jo Shields <jo.shields@xamarin.com>
2014-11-26 17:14:54 +0000

    Set WIX input to output dir from mono build

--- resources/build.bat
+++ resources/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set MONO_VERSION=3.12.1
-set MONO_FILES_DIR=.\mono-files
+set MONO_FILES_DIR=..\tmp\mono
 
 "%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" MonoForWindows.wixproj /p:Configuration=Release /p:DefaultCompressionLevel=high
