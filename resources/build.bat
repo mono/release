99cab25c399bd3d41f9202a5fdb6d369d27558a3
Jo Shields <jo.shields@xamarin.com>
2014-11-26 17:16:13 +0000

    Tweaks to make build.bat run from scripts

--- resources/build.bat
+++ resources/build.bat
@@ -3,4 +3,4 @@
 set MONO_VERSION=3.12.1
 set MONO_FILES_DIR=..\tmp\mono
 
-"%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" MonoForWindows.wixproj /p:Configuration=Release /p:DefaultCompressionLevel=high
+"%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" %~dp0\MonoForWindows.wixproj /p:Configuration=Release /p:DefaultCompressionLevel=high
