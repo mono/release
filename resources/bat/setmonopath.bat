9c9ee89800cf757329df0f8e82f915248ec56196
Jo Shields <jo.shields@xamarin.com>
2016-02-22 16:14:14 +0000

    Bump version in setup

--- resources/bat/setmonopath.bat
+++ resources/bat/setmonopath.bat
@@ -1,4 +1,4 @@
 @echo off
-echo Mono version 4.0.1
+echo Mono version 4.3.2
 echo Prepending '%~dp0' to PATH
 PATH=%~dp0;%PATH%
--- resources/build.bat
+++ resources/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set MONO_VERSION=4.0.1
+set MONO_VERSION=4.3.2
 set MONO_FILES_DIR=..\tmp\mono
 
 "%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" %~dp0\MonoForWindows.wixproj /p:Configuration=Release /p:DefaultCompressionLevel=high
